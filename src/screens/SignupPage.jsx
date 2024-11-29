acc3b99bd23971419e462b3263990976dd72cad6
koraydinnc <koraydincc@icloud.com>
2024-11-29 23:26:24 +0300

    .

--- src/screens/SignupPage.jsx
+++ src/screens/SignupPage.jsx
@@ -1,1 +1,14 @@
-rnfs
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <View>
+      <Text>SignupPage</Text>
+    </View>
+  )
+}
+
+export default SignupPage
+
+const styles = StyleSheet.create({})
